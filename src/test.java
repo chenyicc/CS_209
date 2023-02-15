07502c953b23b3660cb9542c90add0363cfe486a
chenyicc <990315864@qq.com>
2023-02-15 16:24:05 +0800

    first commit

---
+++ src/test.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class test {
+
+    public static void main(String[] args) {
+        Random r = new Random();
+
+        int n = 2000000;
+        System.out.println(n);
+
+        for (int i = 1; i <= n; i++) {
+            int a = r.nextInt(1, 1000000001);
+            System.out.print(a + " ");
+
+
+        }
+    }
+
+
+}
